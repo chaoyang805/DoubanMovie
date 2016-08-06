9aa78f8ea8f9f1fb00967ed41535aeea2f6b0847
chaoyang805 <zhangchaoyang805@gmail.com>
2016-08-06 14:10:27 +0800

    add animations to home screen

--- DoubanMovie/HomeViewController.swift
+++ DoubanMovie/HomeViewController.swift
@@ -17,6 +17,8 @@ class HomeViewController: UIViewController{
 
         movieInfoDialog.ratingInfoView.ratingScore = 5
         movieInfoDialog.addTarget(self, action: #selector(HomeViewController.movieInfoDialogDidTouch(_:)), for: .TouchUpInside)
+        
+        animator = UIDynamicAnimator(referenceView: self.view)
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -29,20 +31,80 @@ class HomeViewController: UIViewController{
     }
     // MARK: - IBActions
     
-    @IBAction func handGesture(sender: UIPanGestureRecognizer) {
+    var animator: UIDynamicAnimator!
+    var attachmentBehavior: UIAttachmentBehavior!
+    var gravityBehavior: UIGravityBehavior!
+    var snapBehavior: UISnapBehavior!
+    
+    @IBAction func handleGestures(sender: UIPanGestureRecognizer) {
+        
+        let location = sender.locationInView(view)
+        let boxLocation = sender.locationInView(movieInfoDialog)
+        let myView = movieInfoDialog
+        
         switch sender.state {
         case .Began:
-            break
+            if let snap = snapBehavior{
+                animator.removeBehavior(snap)
+            }
+            let centerOffset = UIOffset(horizontal: boxLocation.x - myView.bounds.midX, vertical: boxLocation.y - myView.bounds.midY)
+            attachmentBehavior = UIAttachmentBehavior(item: myView, offsetFromCenter: centerOffset, attachedToAnchor: location)
+            attachmentBehavior.frequency = 0
+            animator.addBehavior(attachmentBehavior)
         case .Changed:
-            break
+            attachmentBehavior.anchorPoint = location
         case .Ended:
-            break
+            animator.removeBehavior(attachmentBehavior)
+            snapBehavior = UISnapBehavior(item: myView, snapToPoint: CGPoint(x: view.center.x, y: view.center.y + 20))
+            animator.addBehavior(snapBehavior)
+            
+            let translation = sender.translationInView(view)
+            
+            if abs(translation.x) < 150 {
+                return
+            }
+            animator.removeAllBehaviors()
+            if gravityBehavior == nil {
+                gravityBehavior = UIGravityBehavior(items: [myView])
+            }
+            if translation.x < -150 {
+                // 左
+                gravityBehavior.gravityDirection = CGVector(dx: -20.0, dy: 0)
+            } else if translation.x > 150 {
+                // 右
+                gravityBehavior.gravityDirection = CGVector(dx: 20.0, dy: 0)
+            }
+            animator.addBehavior(gravityBehavior)
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(NSEC_PER_MSEC * 300)), dispatch_get_main_queue(), {
+                self.refreshData()
+            })
         default:
             break
         }
         
     }
     
+    func refreshData() {
+        animator.removeAllBehaviors()
+        snapBehavior = UISnapBehavior(item: movieInfoDialog, snapToPoint: view.center)
+        movieInfoDialog.center = CGPoint(x: view.center.x, y: view.center.y + 20)
+        attachmentBehavior.anchorPoint = CGPoint(x: view.center.x, y: view.center.y + 20)
+        
+        animateShowDialog()
+    }
+    
+    func animateShowDialog() {
+        let scale = CGAffineTransformMakeScale(0.5, 0.5)
+        let offsetX = gravityBehavior.gravityDirection.dx < 0 ? self.view.frame.width + 200 : -200
+        let translation = CGAffineTransformMakeTranslation(offsetX, 0)
+        movieInfoDialog.transform = CGAffineTransformConcat(scale, translation)
+        
+        UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+            self.movieInfoDialog.transform = CGAffineTransformIdentity
+            }, completion: nil)
+    }
+    
+    // MAKR: - Navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
         if segue.identifier == "ShowDetailSegue" {
