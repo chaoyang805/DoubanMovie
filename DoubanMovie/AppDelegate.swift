0506b5507363f02f5798fa87aa585f477a9f29cc
chaoyang805 <zhangchaoyang805@gmail.com>
2016-11-01 17:50:53 +0800

    add 3DTouch shortcut for home icon

--- DoubanMovie/AppDelegate.swift
+++ DoubanMovie/AppDelegate.swift
@@ -21,40 +21,97 @@ import RxAlamofire
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+    enum ShortcutIdentifier: String {
+        case share
+        case favorite
+        case search
         
-//        RxAlamofireService.shared.loadMovies().observeOn(MainScheduler.instance).subscribe(onNext: { (movie) in
-//            NSLog("\(movie)")
-//            }, onError: { (error) in
-//                NSLog("\(error)")
-//            }, onCompleted: nil, onDisposed: nil)
-        return true
+        init?(fullType: String) {
+            guard let last = fullType.components(separatedBy: ".").last else { return nil }
+            self.init(rawValue: last)
+        }
+        
+        var type: String {
+            return Bundle.main.bundleIdentifier! + ".\(self.rawValue)"
+        }
+    }
+    
+    var window: UIWindow?
+    private var launchedShortcutItem: UIApplicationShortcutItem?
+    
+    @discardableResult
+    private func handleShortcutItem(_ shortcutItem: UIApplicationShortcutItem) -> Bool {
+        var handled = false
+        
+        guard ShortcutIdentifier(fullType: shortcutItem.type) != nil else { return false }
+        
+        switch shortcutItem.type {
+        case ShortcutIdentifier.favorite.type:
+            handled = true
+            handleFavoriteShortcut()
+        case ShortcutIdentifier.search.type:
+            handled = true
+            handleSearchShortcut()
+        case ShortcutIdentifier.share.type:
+            handled = true
+            handleShareShortcut()
+        default:
+            break
+        }
+        return handled
+    }
+    
+    private func handleShareShortcut() {
+        guard let keyWindow = UIApplication.shared.keyWindow, let rootVC = keyWindow.rootViewController as? UINavigationController else { return }
+    }
+    
+    private func handleFavoriteShortcut() {
+        guard let keyWindow = UIApplication.shared.keyWindow, let rootVC = keyWindow.rootViewController as? UINavigationController else { return }
+        rootVC.popToRootViewController(animated: false)
+        
+        if let homeVC = rootVC.topViewController as? HomeViewController {
+            homeVC.performSegue(withIdentifier: "FavoritesSegue", sender: rootVC)
+        }
+    }
+    
+    private func handleSearchShortcut() {
+        guard let keyWindow = UIApplication.shared.keyWindow, let rootVC = keyWindow.rootViewController as? UINavigationController else { return }
+        
+        rootVC.popToRootViewController(animated: false)
+        
+        if let homeVC = rootVC.topViewController as? HomeViewController {
+            homeVC.performSegue(withIdentifier: "SearchSegue", sender: rootVC)
+        }
+    }
+    // 程序运行在后台时通过 shortcut 恢复时调用
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        
+        let handledShortcutItem = handleShortcutItem(shortcutItem)
+        completionHandler(handledShortcutItem)
+    }
+    
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        // return false 不调用 applicationPerformAction ，一般在程序通过 shortcut 启动时 return false 然后在 application didBecomeActive
+        var shouldPerformAdditionalDelegateHandling = true
+        if let shortcutItem = launchOptions?[UIApplicationLaunchOptionsKey.shortcutItem] as? UIApplicationShortcutItem {
+            
+            launchedShortcutItem = shortcutItem
+            shouldPerformAdditionalDelegateHandling = false
+        }
+        
+        return shouldPerformAdditionalDelegateHandling
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
+    //  application didFinishLaunchingWithOptions return false 时，说明是通过 shortcut 启动，在这里处理 shortcut
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        guard let shortcutItem = launchedShortcutItem else { return }
+        
+        handleShortcutItem(shortcutItem)
+        launchedShortcutItem = nil
     }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
 
